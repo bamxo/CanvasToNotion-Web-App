f9d3c37e4cf0ecfeb6c56253712e3c4200f1b86b
Juan-Fernando <juprmora@ucsc.edu>
2025-04-28 20:59:53 -0700

    got jest tests working

---
+++ backend/jest.config.js
@@ -0,0 +1,21 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    "^.+\.tsx?$": ["ts-jest",{}],
+  },
+  verbose: true,
+  roots: ['<rootDir>/test/'],
+  testPathIgnorePatterns: ["/node_modules/"],
+  collectCoverage:true,
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global:
+    {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
+};
